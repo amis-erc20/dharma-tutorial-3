c0a971ee029dc8ccc0f620b8644a5d0736aec476
Kayvon Tehranian <ktehranian@gmail.com>
2018-06-18 16:27:46 -0700

    feat: render form in app

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+import { RequestLoanForm } from "./form.js"
+
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <header className="App-header">
-                    <h1 className="App-title">Welcome to React</h1>
+                    <h1 className="App-title">Request a Loan on Dharma</h1>
                 </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+                <RequestLoanForm />
             </div>
         );
     }
