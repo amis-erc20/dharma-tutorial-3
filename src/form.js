345e926673acea40670fd932404662c5d3ccb51d
Kayvon Tehranian <ktehranian@gmail.com>
2018-06-18 16:22:00 -0700

    feat: add add'l form input element

--- src/form.js
+++ src/form.js
@@ -11,7 +11,7 @@ class RequestLoanForm extends Component {
           termLength: undefined,
         };
 
-        this.handleChange = this.handleChange.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -35,9 +35,23 @@ class RequestLoanForm extends Component {
       <form onSubmit={this.handleSubmit}>
         <label>
           Principal Amount:
-          <input name="principal" type="number" value={this.state.principal} onChange={this.handleChange} />
+          <input name="principal" type="number" value={this.state.principal} onChange={this.handleInputChange} />
         </label>
 
+        <label>
+          Collateral Amount:
+          <input name="collateral" type="number" value={this.state.collateral} onChange={this.handleInputChange} />
+        </label>
+
+        <label>
+          Expiration:
+          <input name="expiration" type="number" value={this.state.expiration} onChange={this.handleInputChange} />
+        </label>
+
+        <label>
+          Term Length:
+          <input name="termLength" type="number" value={this.state.termLength} onChange={this.handleInputChange} />
+        </label>
 
         <input type="submit" value="Submit" />
       </form>
