5bdfe3cdcc965d67578fdf421df3265b01654115
Graeme <graemelearn@protonmail.com>
2018-06-18 18:18:32 -0700

    style: labels

--- src/form.js
+++ src/form.js
@@ -1,86 +1,80 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 export class RequestLoanForm extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-          principal: undefined,
-          collateral: undefined,
-          expiration: undefined,
-          termLength: undefined,
+            principal: undefined,
+            collateral: undefined,
+            expiration: undefined,
+            termLength: undefined,
         };
 
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-  }
+    }
 
-  handleInputChange(event) {
-      const target = event.target;
-      const value = target.value;
-      const name = target.name;
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
 
-      this.setState({
-        [name]: value
-      });
-  }
+        this.setState({
+            [name]: value
+        });
+    }
 
-  handleSubmit(event) {
-      console.log(this.state);
-      event.preventDefault();
-  }
+    handleSubmit(event) {
+        console.log(this.state);
+        event.preventDefault();
+    }
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className="request-form">
-          <div className="request-form-group">
-            <label>
-              Principal Amount:
-              <input className="request-form-input"
-                     name="principal"
-                     type="number"
-                     value={this.state.principal}
-                     onChange={this.handleInputChange} />
-            </label>
-          </div>
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit} className="request-form">
+                <div className="request-form-group">
+                    <label className="request-form-label">Principal Amount:</label>
+                    <input className="request-form-input"
+                           name="principal"
+                           type="number"
+                           value={this.state.principal}
+                           onChange={this.handleInputChange}/>
 
-        <div className="request-form-group">
-            <label>
-              Collateral Amount:
-              <input className="request-form-input"
-                     name="collateral"
-                     type="number"
-                     value={this.state.collateral}
-                     onChange={this.handleInputChange} />
-            </label>
-        </div>
+                </div>
 
-        <div className="request-form-group">
-            <label>
-              Expiration:
-              <input className="request-form-input"
-                     name="expiration"
-                     type="number"
-                     value={this.state.expiration}
-                     onChange={this.handleInputChange} />
-            </label>
-        </div>
+                <div className="request-form-group">
+                    <label className="request-form-label">Collateral Amount:</label>
+                    <input className="request-form-input"
+                           name="collateral"
+                           type="number"
+                           value={this.state.collateral}
+                           onChange={this.handleInputChange}/>
 
-        <div className="request-form-group">
-            <label>
-              Term Length:
-              <input className="request-form-input"
-                     name="termLength"
-                     type="number"
-                     value={this.state.termLength}
-                     onChange={this.handleInputChange} />
-            </label>
-        </div>
+                </div>
 
-        <div className="request-form-group">
-            <input type="submit" value="Submit" className="request-form-button" />
-        </div>
-      </form>
-    );
-  }
+                <div className="request-form-group">
+                    <label className="request-form-label">Expiration:</label>
+                    <input className="request-form-input"
+                           name="expiration"
+                           type="number"
+                           value={this.state.expiration}
+                           onChange={this.handleInputChange}/>
+                </div>
+
+                <div className="request-form-group">
+                    <label className="request-form-label">Term Length:</label>
+                    <input className="request-form-input"
+                           name="termLength"
+                           type="number"
+                           value={this.state.termLength}
+                           onChange={this.handleInputChange}/>
+                </div>
+
+                <div className="request-form-group">
+                    <input type="submit" value="Submit" className="request-form-button"/>
+                </div>
+            </form>
+        );
+    }
 }
