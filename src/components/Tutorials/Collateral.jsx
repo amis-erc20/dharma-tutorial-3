69b92ed7328fb66cfeb6e1213b7f2a17567a78fc
Chris Min <chris@dharma.io>
2018-07-05 12:58:49 -0700

    feat: disable return and seize collateral buttons appropriately

--- src/components/Tutorials/Collateral.jsx
+++ src/components/Tutorials/Collateral.jsx
@@ -6,8 +6,32 @@ export default class Collateral extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            collateralReturnable: false,
+            collateralSeizable: false
+        };
+
         this.handleReturnCollateral = this.handleReturnCollateral.bind(this);
         this.handleSeizeCollateral = this.handleSeizeCollateral.bind(this);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const { debtOrder } = nextProps;
+
+        if (!debtOrder) {
+            return {};
+        }
+
+        this.getLoanSummary(debtOrder).then(data => {
+            this.setState(data);
+        });
+    }
+
+    async getLoanSummary(debtOrder) {
+        const collateralReturnable = await debtOrder.isCollateralReturnable();
+        const collateralSeizable = await debtOrder.isCollateralSeizable();
+
+        return { collateralReturnable, collateralSeizable };
     }
 
     async handleReturnCollateral(event) {
@@ -31,13 +55,23 @@ export default class Collateral extends Component {
     }
 
     render() {
+        const { collateralReturnable, collateralSeizable } = this.state;
+
         return (
-            <div className="FillTutorial container Tutorial" id="fill-loan">
+            <div className="CollateralTutorial container Tutorial" id="fill-loan">
                 <header className="App-header">
                     <h3 className="App-title">Return or Seize Collateral</h3>
                 </header>
-                <Button label={"Seize Collateral"} disabled={false} onClick={this.handleSeizeCollateral} />
-                <Button label={"Return Collateral"} disabled={false} onClick={this.handleReturnCollateral} />
+                <Button
+                    label={"Return Collateral"}
+                    disabled={!collateralReturnable}
+                    onClick={this.handleReturnCollateral}
+                />
+                <Button
+                    label={"Seize Collateral"}
+                    disabled={!collateralSeizable}
+                    onClick={this.handleSeizeCollateral}
+                />
             </div>
         );
     }
