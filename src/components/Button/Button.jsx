b54708f26c6e34583fe0244fbf7e593df28a4f27
Chris Min <chris@dharma.io>
2018-07-05 16:42:11 -0700

    fix: implement Button as div

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,21 +1,15 @@
 import React, { Component } from "react";
 
-// TODO: refactor to remove the form tag
+import "./Button.css";
+
 export class Button extends Component {
     render() {
         const { disabled, label, onClick } = this.props;
 
         return (
-            <form className="request-form" onSubmit={onClick}>
-                <div className="form-group">
-                    <input
-                        type="submit"
-                        value={label}
-                        className="btn btn-primary request-form-submit"
-                        disabled={disabled}
-                    />
-                </div>
-            </form>
+            <div className="button btn btn-primary" disabled={disabled} onClick={onClick}>
+                {label}
+            </div>
         );
     }
 }
