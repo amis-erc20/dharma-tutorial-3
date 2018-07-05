c55eed55bbf8372b47daedcd095e1cd7f7b72cc6
Kayvon Tehranian <ktehranian@gmail.com>
2018-07-05 16:50:15 -0700

    feat: render collateral summary component in UI

--- src/components/TutorialStatus/TutorialStatus.jsx
+++ src/components/TutorialStatus/TutorialStatus.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Balances from "../Balances/Balances";
 import LoanSummary from "../LoanSummary/LoanSummary";
 import RepaymentSummary from "../RepaymentSummary/RepaymentSummary";
+import CollateralSummary from "../CollateralSummary/CollateralSummary"
 
 import "./TutorialStatus.css";
 
@@ -13,6 +14,7 @@ export default class TutorialStatus extends Component {
             <div className="TutorialStatus">
                 <LoanSummary debtOrder={debtOrder} />
                 <RepaymentSummary debtOrder={debtOrder} debtOrderFilled={debtOrderFilled} />
+                <CollateralSummary debtOrder={debtOrder} debtOrderFilled={debtOrderFilled} />
                 <Balances balances={balances} />
             </div>
         );
