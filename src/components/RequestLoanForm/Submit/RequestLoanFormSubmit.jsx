1eb746f428a38663b40933d6b69aa3345612a597
Chris Min <chris@dharma.io>
2018-07-05 16:41:32 -0700

    fix: delete unused css files

--- src/components/RequestLoanForm/Submit/RequestLoanFormSubmit.jsx
+++ src/components/RequestLoanForm/Submit/RequestLoanFormSubmit.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 
-import "./RequestLoanFormSubmit.css";
-
 export default class RequestLoanFormSubmit extends Component {
     render() {
         const { disabled } = this.props;
 
         return (
             <div className="request-form-group">
-                <input type="submit" value="Submit" className="btn btn-primary request-form-submit" disabled={disabled} />
+                <input
+                    type="submit"
+                    value="Submit"
+                    className="btn btn-primary request-form-submit"
+                    disabled={disabled}
+                />
             </div>
         );
     }
